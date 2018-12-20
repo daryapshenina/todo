f57c5f39e7dc793ae3314bb2c6dd33dc930cc854
psheninada <psheninada@altarix.ru>
2018-12-20 14:53:43 +0400

    Изменила css для меню

--- html/testtodo/src/components/Menu.js
+++ html/testtodo/src/components/Menu.js
@@ -7,6 +7,7 @@
 import React, { PropTypes, Component } from 'react'
 import styled from 'styled-components';
 import {Link, NavLink} from 'react-router-dom'
+import { SplitButton, MenuItem } from 'react-bootstrap';
 
 
 /*
@@ -17,14 +18,43 @@ const isActiveFunc = (match, location) => {
     return match;
 };
 
+
+const Nav = styled.nav`
+   
+.navlink{
+padding:10px;}
+`;
+
+/*class Menu extends Component {
+    render() {
+        return (
+            <div className='pagewrap'>
+            <div className = "menu">
+            <Nav>
+            <NavLink className="navlink" exact activeClassName='active' to='/'>Главная</NavLink>
+            <NavLink className="navlink" isActive={isActiveFunc} activeClassName='active' to='/user'>Пользователи</NavLink>
+            <NavLink className="navlink" isActive={isActiveFunc} activeClassName='active' to='/weather'>Погода</NavLink>
+            <NavLink className="navlink"  isActive={isActiveFunc} activeClassName='active' to='/list'>Лист</NavLink>
+            <NavLink className="navlink" isActive={isActiveFunc} activeClassName='active' to='/dropping'>Попытка переносимых элементов</NavLink>
+            </Nav>
+            </div>
+        </div>
+
+    )
+    }
+}
+export default Menu;*/
+
+
 class Menu extends Component {
     render() {
         return (
-            <nav>
-            <NavLink exact activeClassName='active' to='/'>Главная</NavLink>
-            <NavLink isActive={isActiveFunc} activeClassName='active' to='/user'>Пользователи</NavLink>
-            <NavLink isActive={isActiveFunc} activeClassName='active' to='/weather'>Погода</NavLink>
-            <NavLink isActive={isActiveFunc} activeClassName='active' to='/list'>Лист</NavLink>
+            <nav className="menu">
+            <NavLink className = "menu-item" exact activeClassName='active' to='/'>Главная</NavLink>
+            <NavLink className = "menu-item" isActive={isActiveFunc} activeClassName='active' to='/user'>Пользователи</NavLink>
+            <NavLink className = "menu-item" isActive={isActiveFunc} activeClassName='active' to='/weather'>Погода</NavLink>
+            <NavLink className = "menu-item"  isActive={isActiveFunc} activeClassName='active' to='/list'>Лист</NavLink>
+            <NavLink className = "menu-item" isActive={isActiveFunc} activeClassName='active' to='/dropping'>Попытка переносимых элементов</NavLink>
             </nav>
     )
     }
